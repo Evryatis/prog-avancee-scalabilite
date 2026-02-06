7a2d387670f3e431192612fc687ec4709874ad31
Evryatis <48336818+Evryatis@users.noreply.github.com>
2026-02-06 16:35:53 +0100

    Ajout et séparation des tests de scalabilité

--- EstimationPi/Pi.java
+++ EstimationPi/Pi.java
@@ -38,7 +38,7 @@ class Master {
 	List<Callable<Long>> tasks = new ArrayList<Callable<Long>>();
 	for (int i = 0; i < numWorkers; ++i) 
 	    {
-		tasks.add(new Worker(totalCount));
+		tasks.add(new Worker(totalCount/numWorkers));
 	    }
     
 	// Run them and receive a collection of Futures
@@ -53,18 +53,18 @@ class Master {
 		// until result from corresponding worker is ready.
 		total += f.get();
 	    }
-	double pi = 4.0 * total / totalCount / numWorkers;
+	//double pi = 4.0 * total / totalCount / numWorkers;
 
-	long stopTime = System.nanoTime();
+	//long stopTime = System.nanoTime();
 
-	System.out.println("\nPi : " + pi );
-	System.out.println("Error: " + (Math.abs((pi - Math.PI)) / Math.PI) +"\n");
+	//System.out.println("\nPi : " + pi );
+	//System.out.println("Error: " + (Math.abs((pi - Math.PI)) / Math.PI) +"\n");
 
-	System.out.println("Ntot: " + totalCount*numWorkers);
-	System.out.println("Available processors: " + numWorkers);
-	System.out.println("Time Duration (ms): " + (stopTime - startTime) + "\n");
+	//System.out.println("Ntot: " + totalCount*numWorkers);
+	//System.out.println("Available processors: " + numWorkers);
+	//System.out.println("Time Duration (ms): " + (stopTime - startTime) + "\n");
 
-	System.out.println( (Math.abs((pi - Math.PI)) / Math.PI) +" "+ totalCount*numWorkers +" "+ numWorkers +" "+ (stopTime - startTime));
+	//System.out.println( (Math.abs((pi - Math.PI)) / Math.PI) +" "+ totalCount*numWorkers +" "+ numWorkers +" "+ (stopTime - startTime));
 
 	exec.shutdown();
 	return total;
